ea8d29b11ad43d2ec459d1bb552139201d55a43f
HH <hans.herzl@live.com>
2025-04-27 19:58:39 -0600

    Implementación por defecto para envío de correos electrónicos por SMTP

--- Source/InvoizR/InvoizR.Infrastructure/Clients/SatanCitySmtpClient.cs
+++ Source/InvoizR/InvoizR.Infrastructure/Clients/SatanCitySmtpClient.cs
@@ -1,14 +1,19 @@
-﻿using System.Net.Mail;
+﻿using System.Net;
+using System.Net.Mail;
 using InvoizR.Clients;
 using InvoizR.Clients.Contracts;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace InvoizR.Infrastructure.Clients;
 
 public class SatanCitySmtpClient : ISmtpClient
 {
-    public SatanCitySmtpClient(IOptions<SmtpClientSettings> options)
+    private readonly ILogger _logger;
+
+    public SatanCitySmtpClient(ILogger<SatanCitySmtpClient> logger, IOptions<SmtpClientSettings> options)
     {
+        _logger = logger;
         ClientSettings = options.Value;
     }
 
@@ -16,6 +21,24 @@ public class SatanCitySmtpClient : ISmtpClient
 
     public void Send(MailMessage mailMessage)
     {
-        // TODO: Emulate mail sending...
+        _logger?.LogInformation($"Sending email to {string.Join(",", mailMessage.To.Select(item => item.Address))}...");
+
+        try
+        {
+            using var client = new SmtpClient(ClientSettings.Host, ClientSettings.Port)
+            {
+                EnableSsl = ClientSettings.EnableSsl,
+                UseDefaultCredentials = ClientSettings.UseDefaultCredentials,
+                Credentials = new NetworkCredential(ClientSettings.UserName, ClientSettings.Password),
+                DeliveryMethod = SmtpDeliveryMethod.Network
+            };
+
+            // TODO: uncomment until SMTP is available...
+            //client.Send(mailMessage);
+        }
+        catch (Exception ex)
+        {
+            _logger?.LogCritical(ex, $"Error sending email on SatanCitySmtpClient");
+        }
     }
 }
