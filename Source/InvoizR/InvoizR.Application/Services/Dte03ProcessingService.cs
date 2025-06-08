b3a3075e983697261251ecfd0075309962ded02d
HH <hans.herzl@live.com>
2025-06-07 23:39:37 -0600

    Abstracción de clases para procesamiento de DTEs. #34

--- Source/InvoizR/InvoizR.Application/Services/Dte03ProcessingService.cs
+++ Source/InvoizR/InvoizR.Application/Services/Dte03ProcessingService.cs
@@ -1,22 +1,20 @@
-﻿using InvoizR.Application.Common.Contracts;
-using InvoizR.Application.Helpers;
+﻿using InvoizR.Application.Helpers;
 using InvoizR.Domain.Entities;
-using InvoizR.Domain.Enums;
 using InvoizR.SharedKernel.Mh.FeCcf;
 using Microsoft.Extensions.Logging;
 
 namespace InvoizR.Application.Services;
 
-public class Dte03ProcessingService
+public class Dte03ProcessingService : DteProcessingService
 {
     private readonly ILogger _logger;
 
     public Dte03ProcessingService(ILogger<Dte03ProcessingService> logger)
+        : base(logger)
     {
-        _logger = logger;
     }
 
-    private bool Init(Invoice invoice)
+    protected override bool Init(Invoice invoice)
     {
         FeCcfv3 dte;
 
@@ -40,42 +38,5 @@ public class Dte03ProcessingService
         invoice.Serialization = dte.ToJson();
 
         return true;
-    }
-
-    public async Task SetInvoiceAsInitializedAsync(long? invoiceId, IInvoizRDbContext dbContext, CancellationToken cancellationToken = default)
-    {
-        var inv = await dbContext.GetInvoiceAsync(invoiceId, includes: true, tracking: true, ct: cancellationToken);
-
-        var invType = await dbContext.GetInvoiceTypeAsync(inv.InvoiceTypeId, ct: cancellationToken);
-
-        var dteInfo = DteInfoHelper.Get(invType.Id, inv.Pos.Branch.EstablishmentPrefix, inv.Pos.Branch.TaxAuthId, inv.Pos.Code, inv.InvoiceNumber);
-
-        inv.SchemaType = invType.SchemaType;
-        inv.SchemaVersion = invType.SchemaVersion;
-        inv.GenerationCode = dteInfo.GenerationCode;
-        inv.ControlNumber = dteInfo.ControlNumber;
-        inv.ProcessingStatusId = (short)InvoiceProcessingStatus.Initialized;
-
-        dbContext.InvoiceProcessingStatusLog.Add(new(inv.Id, inv.ProcessingStatusId));
-
-        _logger.LogInformation($" Updating '{inv.InvoiceNumber}' invoice: '{inv.SchemaType}{inv.SchemaVersion}', '{inv.GenerationCode}', '{inv.ControlNumber}'...");
-
-        await dbContext.SaveChangesAsync(cancellationToken);
-    }
-
-    public async Task SetInvoiceAsRequestedAsync(long? invoiceId, IInvoizRDbContext dbContext, CancellationToken cancellationToken = default)
-    {
-        var inv = await dbContext.GetInvoiceAsync(invoiceId, tracking: true, ct: cancellationToken);
-
-        if (Init(inv))
-            inv.ProcessingStatusId = (short)InvoiceProcessingStatus.Requested;
-        else
-            inv.ProcessingStatusId = (short)InvoiceProcessingStatus.InvalidSchema;
-
-        dbContext.InvoiceProcessingStatusLog.Add(new(inv.Id, inv.ProcessingStatusId));
-
-        _logger.LogInformation($" Updating '{inv.InvoiceNumber}' invoice: '{inv.SchemaType}{inv.SchemaVersion}', '{inv.GenerationCode}', '{inv.ControlNumber}'...");
-
-        await dbContext.SaveChangesAsync(cancellationToken);
     }
 }
---
+++ Source/InvoizR/InvoizR.Application/Services/Models/CreateDteRequest.cs
@@ -0,0 +1,23 @@
+﻿using InvoizR.Application.Common;
+
+namespace InvoizR.Application.Services.Models;
+
+public record CreateDteRequest
+{
+    public CreateDteRequest()
+    {
+    }
+
+    public CreateDteRequest(MhSettings mhSettings, ProcessingSettings processingSettings, string jwt, long? invoiceId)
+    {
+        MhSettings = mhSettings;
+        ProcessingSettings = processingSettings;
+        Jwt = jwt;
+        InvoiceId = invoiceId;
+    }
+
+    public MhSettings MhSettings { get; set; }
+    public ProcessingSettings ProcessingSettings { get; set; }
+    public string Jwt { get; set; }
+    public long? InvoiceId { get; set; }
+}
