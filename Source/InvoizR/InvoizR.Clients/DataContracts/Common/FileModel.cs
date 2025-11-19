130e87ae49494d310c4ef91b37e57297b19510bc
HH <hans.herzl@live.com>
2025-11-18 23:53:43 -0600

    Agregar clase base para archivo de factura y contingencia

---
+++ Source/InvoizR/InvoizR.Clients/DataContracts/Common/FileModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using InvoizR.SharedKernel;
+
+namespace InvoizR.Clients.DataContracts.Common;
+
+public record FileModel
+{
+    public long? Size { get; set; }
+    public string MimeType { get; set; }
+    public string FileName { get; set; }
+    public string ExternalUrl { get; set; }
+    public DateTime? CreatedAt { get; set; }
+
+    public string FileType { get; set; }
+
+    public bool IsJson
+        => string.Compare(FileType, Tokens.Json, true) == 0;
+
+    public bool IsPdf
+        => string.Compare(FileType, Tokens.Pdf, true) == 0;
+
+    public byte[] File { get; set; }
+
+    public string GetJson()
+        => Encoding.Default.GetString(File);
+
+    public string GetPdf()
+        => Convert.ToBase64String(File);
+}
--- Source/InvoizR/InvoizR.Clients/DataContracts/InvoiceFileItemModel.cs
+++ Source/InvoizR/InvoizR.Clients/DataContracts/InvoiceFileItemModel.cs
@@ -1,16 +1,7 @@
-﻿namespace InvoizR.Clients.DataContracts;
+﻿using InvoizR.Clients.DataContracts.Common;
 
-public record InvoiceFileItemModel
+namespace InvoizR.Clients.DataContracts;
+
+public record InvoiceFileItemModel : FileModel
 {
-    public InvoiceFileItemModel()
-    {
-    }
-
-    public long? Size { get; set; }
-    public string MimeType { get; set; }
-    public string FileType { get; set; }
-    public string FileName { get; set; }
-    public string ExternalUrl { get; set; }
-    public DateTime? CreatedAt { get; set; }
-    public byte[] File { get; set; }
 }
