747219aaab4978f3f4212c89be9570393e86a6dd
HH <hans.herzl@live.com>
2025-11-24 00:42:13 -0600

    Agregar componente razor para visualizar DTE-04

--- Source/InvoizR/InvoizR.Clients/DataContracts/Invoices/InvoiceDetailsModel.cs
+++ Source/InvoizR/InvoizR.Clients/DataContracts/Invoices/InvoiceDetailsModel.cs
@@ -1,5 +1,6 @@
 ﻿using InvoizR.SharedKernel.Mh.FeCcf;
 using InvoizR.SharedKernel.Mh.FeFc;
+using InvoizR.SharedKernel.Mh.FeNr;
 
 namespace InvoizR.Clients.DataContracts.Invoices;
 
@@ -53,4 +54,7 @@ public static class InvoiceDetailsExtensions
 
     public static bool IsDte03(this InvoiceDetailsModel detailsModel)
         => detailsModel.InvoiceTypeId == FeCcfv3.TypeId;
+
+    public static bool IsDte04(this InvoiceDetailsModel detailsModel)
+        => detailsModel.InvoiceTypeId == FeNrv3.TypeId;
 }
